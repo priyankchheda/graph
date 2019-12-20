844efb102759ea3f3c8539fc1b9a5a5e464cd13c
x899 <p.chheda29@gmail.com>
2019-12-20 11:34:09 +0530

    linting

--- dfs.py
+++ dfs.py
@@ -1,23 +1,29 @@
 from graph import Graph
 
 
-def dfs(g, start):
+def dfs(g_obj, start):
     visited, stack = set(), [start]
     while stack:
         vertex = stack.pop()
         if vertex not in visited:
             visited.add(vertex)
-            for nextNode in g.neighbours(vertex):
-                if nextNode not in visited:
-                    stack.append(nextNode)
+            for next_node in g_obj.neighbours(vertex):
+                if next_node not in visited:
+                    stack.append(next_node)
     return visited
 
 
-g = {
-    '1': ['2', '3'],
-    '2': ['3', '1'],
-    '3': ['1', '2', '4'],
-    '4': ['3']
-}
-g = Graph(g)
-print(dfs(g, '1'))
+def main():
+    """ operational function """
+    g_dict = {
+        '1': ['2', '3'],
+        '2': ['3', '1'],
+        '3': ['1', '2', '4'],
+        '4': ['3']
+    }
+    g_obj = Graph(g_dict)
+    print(dfs(g_obj, '1'))
+
+
+if __name__ == "__main__":
+    main()
--- paths.py
+++ paths.py
@@ -1,56 +1,63 @@
 from graph import Graph
 
 
-def find_path(g, start, end, path=[]):
+def find_path(g_obj, start, end, path=[]):
     path = path + [start]
     if start == end:
         return path
-    if not g.is_vertex(start):
+    if not g_obj.is_vertex(start):
         return None
-    for node in g.neighbours(start):
+    for node in g_obj.neighbours(start):
         if node not in path:
-            newpath = find_path(g, node, end, path)
+            newpath = find_path(g_obj, node, end, path)
             return newpath
     return None
 
 
-def find_all_path(g, start, end, path=[]):
+def find_all_path(g_obj, start, end, path=[]):
     path = path + [start]
     if start == end:
         return [path]
-    if not g.is_vertex(start):
+    if not g_obj.is_vertex(start):
         return None
     paths = []
-    for node in g.neighbours(start):
+    for node in g_obj.neighbours(start):
         if node not in path:
-            newpaths = find_all_path(g, node, end, path)
+            newpaths = find_all_path(g_obj, node, end, path)
             for newpath in newpaths:
                 paths.append(newpath)
     return paths
 
 
-def find_shortest_path(g, start, end, path=[]):
+def find_shortest_path(g_obj, start, end, path=[]):
     path = path + [start]
     if start == end:
         return path
-    if not g.is_vertex(start):
+    if not g_obj.is_vertex(start):
         return None
     shortest = None
-    for node in g.neighbours(start):
+    for node in g_obj.neighbours(start):
         if node not in path:
-            newpath = find_shortest_path(g, node, end, path)
+            newpath = find_shortest_path(g_obj, node, end, path)
             if newpath:
                 if not shortest or len(newpath) < len(shortest):
                     shortest = newpath
     return shortest
 
-g = {
-    '1': ['2', '3'],
-    '2': ['3', '1'],
-    '3': ['1', '2', '4'],
-    '4': ['3']
-}
-g = Graph(g)
-print(find_path(g, '1', '3'))
-print(find_all_path(g, '1', '3'))
-print(find_shortest_path(g, '1', '3'))
+
+def main():
+    """ operational function """
+    g_dict = {
+        '1': ['2', '3'],
+        '2': ['3', '1'],
+        '3': ['1', '2', '4'],
+        '4': ['3']
+    }
+    g_obj = Graph(g_dict)
+    print(find_path(g_obj, '1', '3'))
+    print(find_all_path(g_obj, '1', '3'))
+    print(find_shortest_path(g_obj, '1', '3'))
+
+
+if __name__ == "__main__":
+    main()
